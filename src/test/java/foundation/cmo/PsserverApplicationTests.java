097855c049c0409dad47e305da0f6369dc2d2223
Marcelo <mlsilva@mlsilva.local>
2023-01-25 21:22:06 -0300

    Estrutura inicial

---
+++ src/test/java/foundation/cmo/PsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package foundation.cmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
