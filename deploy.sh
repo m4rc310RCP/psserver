10115361bccd7c3d558366fcbdfe40f3ec89d258
Marcelo <mlsilva@mlsilva.local>
2023-01-30 18:33:06 -0300

    Feito deploy para servidor dokku

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git add . && git commit -m 'Testes de deploy dokku' && git push -f dokku master && git push -f origin master
+git add . && git commit -m 'Feito deploy para servidor dokku' && git push -f dokku master && git push -f origin master
 
